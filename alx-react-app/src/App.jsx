65ac7be54883480da0042de20ed1a19147dd173f
Mokete <ematyabekana@gmail.com>
2024-11-01 20:05:19 +0200

    Updated the name of the Main component to MainContent.

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
-import Main from './components/MainContent'
+import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 
 function App() {
@@ -36,7 +36,7 @@ function App() {
       <WelcomeMessage />
 
        <Header/>
-       <Main/>
+       <MainContent/>
        <Footer/>
     </>
     
