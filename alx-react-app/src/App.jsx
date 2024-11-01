6e9024566cf1dc4581b3685137f271efc8f9aa6e
Mokete <ematyabekana@gmail.com>
2024-11-01 20:10:42 +0200

    Updated the conponents inside the App component.

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import WelcomeMessage from './components/WelcomeMessage'
-import Header from './components/Header'
-import MainContent from './components/MainContent'
-import Footer from './components/Footer'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -34,15 +34,11 @@ function App() {
       </p>
 
       <WelcomeMessage />
-
-       <Header/>
-       <MainContent/>
-       <Footer/>
+      <Header/>
+      <MainContent/>
+      <Footer/>
     </>
-    
-   
   );
-  
 }
 
-export default App
+export default App;
