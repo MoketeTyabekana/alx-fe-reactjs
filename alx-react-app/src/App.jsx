d17258cb8a04c08edc24c4f9147028734bb45101
Mokete <ematyabekana@gmail.com>
2024-11-01 19:57:27 +0200

    Created a component for the main content and imported it to the App.jsx file.

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
+import Main from './components/MainContent'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -34,6 +35,7 @@ function App() {
       <WelcomeMessage />
 
        <Header/>
+       <Main/>
     </>
     
    
