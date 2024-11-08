8aa6716c238ca20258ea041cd41e46b7651719cf
Mokete <ematyabekana@gmail.com>
2024-11-08 19:54:25 +0200

    Created an inline style for the main component,changing the background color, text color and added padding.

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,6 +1,6 @@
 export default function MainContent(){
     return(
-        <main>
+        <main style={{padding:'10px',color:'#000000', textAlign:'center',backgroundColor:'#fff'}}>
   <p>I love to visit New York, Paris, and Tokyo.</p>
 </main>
     );
