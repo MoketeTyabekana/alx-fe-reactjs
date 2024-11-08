85bd99991bf86c7ae1378ac02003c65f2072b344
Mokete <ematyabekana@gmail.com>
2024-11-08 20:55:51 +0200

    Added the some inline styling to the counter component.

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -5,8 +5,8 @@ function Counter(){
   const [count, setCount]=useState(0);
 
   return(
-    <div>
-    <p>Current Count: {count}</p>
+    <div style={{backgroundColor:'#fff', display:'block',padding:'10px',justifyContent:'center'}}>
+    <p style={{color:'#000000'}}>Current Count: {count}</p>
     <button onClick={() => setCount(count + 1)}>Increment</button>
     <button onClick={() => setCount(count - 1)}>Decrement</button>
     <button onClick={() => setCount(0)}>Reset</button>
