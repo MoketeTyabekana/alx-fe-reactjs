f53d4575df78dedfe50dfe2ff37c02a6a0992d36
Mokete <emtyabekana@gmail.com>
2024-11-10 11:18:49 +0200

    FIxed the path of the impoer for UserContext inside the UserDetails

--- alx-react-app-props/src/components/UserDetails.jsx
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -1,15 +1,13 @@
 import React, { useContext } from 'react';
-import UserContext from './UserContext';
+import UserContext from '../UserContext';
 
-export default function UserDetails({ UserContext }) {
-
+export default function UserDetails() {
     const user = useContext(UserContext);
+
     return (
       <div>
         <p>Name: {user.name}</p>
         <p>Email: {user.email}</p>
       </div>
     );
-  }
-  
-  
+}
