ae7b4d30a0846deb29007026b7da462a451084f2
Mokete <emtyabekana@gmail.com>
2024-11-17 12:46:53 +0200

    Added a link to vaigate to Recipe details.

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,18 +1,21 @@
-// RecipeList component
+import RecipeDetails from './RecipeDetails';
 import { useRecipeStore } from './recipeStore';
+import { Link } from 'react-router-dom'; // Import Link
 
 const RecipeList = () => {
-  const filterRecipes = useRecipeStore(state => state.filterRecipes);
+  const filterRecipes = useRecipeStore((state) => state.filteredRecipes); // Assuming "filteredRecipes" is the correct store key
 
-  if(filterRecipes.length===0){
-    return <p>NO Recipes Found.</p>
+  if (filterRecipes.length === 0) {
+    return <p>No Recipes Found.</p>;
   }
 
   return (
     <div>
-      {filterRecipes.map(recipe => (
+      {filterRecipes.map((recipe) => (
         <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
+          <h3>
+            <Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link> {<RecipeDetails/>}
+          </h3>
           <p>{recipe.description}</p>
         </div>
       ))}
