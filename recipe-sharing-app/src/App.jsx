58721d7aba8bb7302e0112771b4d6af4f3896769
Mokete <emtyabekana@gmail.com>
2024-11-17 11:26:23 +0200

    Added a Route to the Aapp.jsx file for navigation.

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,15 +2,23 @@ import { useState } from 'react';
 import './App.css';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import RecipeDetails from './components/RecipeDetails';
 
 function App() {
 
   return (
     <>
+
       <div>
+        <Router>
+          <Routes>
+            <Route path='/' element={<RecipeDetails/>}  />
+            <Route path='/recipe/:id' element={<RecipeDetails/>}  />
+          </Routes>
+        </Router>
         <AddRecipeForm/>
         <RecipeList/>
-
       </div>
     </>
   )
