60ca0bed89a7587f6b77399b337c36ee018da71d
Mokete <emtyabekana@gmail.com>
2024-11-17 11:10:02 +0200

    Created  a componet for Recipe details, and also taking a recipe Id as a prop.

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,16 +1,21 @@
-// RecipeDetails component
+// RecipeDetails.js
 import { useRecipeStore } from './recipeStore';
+import EditRecipeForm from './EditRecipeForm';
+import DeleteRecipeButton from './DeleteRecipeButton';
 
 const RecipeDetails = ({ recipeId }) => {
-  const recipe = useRecipeStore(state =>
-    state.recipes.find(recipe => recipe.id === recipeId)
+  const recipe = useRecipeStore((state) =>
+    state.recipes.find((recipe) => recipe.id === recipeId)
   );
+
+  if (!recipe) return <p>Recipe not found!</p>;
 
   return (
     <div>
       <h1>{recipe.title}</h1>
       <p>{recipe.description}</p>
-      {/* Render EditRecipeForm and DeleteRecipeButton here */}
+      <EditRecipeForm recipe={recipe} />
+      <DeleteRecipeButton recipeId={recipeId} />
     </div>
   );
 };
