a877c540c78c79122f2151fe8799e02174a7cc7e
Mokete <ematyabekana@gmail.com>
2024-11-22 19:06:43 +0200

    declare the useState variables, and created a form with name,email,password and button inputs.

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,8 +1,19 @@
 import { useState } from "react";
 
 export default function RegistrationForm (){
-    return(
 
+    const [formData,setFormData]=useState({name:'',email:'',password:''});
+
+
+
+    return(
+<form action="">
+    <input type="text" name="name" value={} />
+    <input type="text" name="email" value={} />
+    <input type="text" name="password" value={} />
+
+    <button type="submit">Submit</button>
+</form>
     );
 
 }
