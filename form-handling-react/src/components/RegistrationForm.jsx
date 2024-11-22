834f9922da0398a3f16baa818de0d6b3f268e369
Mokete <ematyabekana@gmail.com>
2024-11-22 20:05:44 +0200

    Defined a useState for errors and created a function to check the user inputs.

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,32 +1,59 @@
 import { useState } from "react";
 
-export default function RegistrationForm (){
+export default function RegistrationForm() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
 
-    const [username,setUsername]=useState('');
-    const [email,setEmail]=useState('');
-    const [password,setPassword]=useState('');
 
-    const handleusernameChange=(e)=>{
-        setUsername(e.target.value);
-    }
+  const validateField=(name,value)=>{
+    let error='';
 
-    const handleEmailChange=(e)=>{
-        setEmail(e.target.value);
-    }
+   
+  }
 
-    const handlePasswordChange=(e)=>{
-        setPassword(e.target.value);
-    }
+  const handleusernameChange = (e) => {
+    setUsername(e.target.value);
+  };
 
-    return(
-<form action="">
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
 
-    <input type="text" name="username" value={username} onChange={handleusernameChange} required/>
-    <input type="text" name="email" value={email} onChange={handleEmailChange} required/>
-    <input type="text" name="password" value={password} onChange={handlePasswordChange} required />
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
 
-    <button type="submit">Submit</button>
-</form>
-    );
+  return (
+    <form action="">
+      <input
+        type="text"
+        name="username"
+        value={username}
+        onChange={handleusernameChange}
+        required
+      />
+      <input
+        type="text"
+        name="email"
+        value={email}
+        onChange={handleEmailChange}
+        required
+      />
+      <input
+        type="text"
+        name="password"
+        value={password}
+        onChange={handlePasswordChange}
+        required
+      />
 
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
