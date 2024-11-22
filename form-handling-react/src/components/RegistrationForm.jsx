04422a304d7d67515a3d2fdaf8472fc767ac0589
Mokete <ematyabekana@gmail.com>
2024-11-22 19:42:04 +0200

    defined the useState hook for each input individually

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -2,24 +2,18 @@ import { useState } from "react";
 
 export default function RegistrationForm (){
 
-    const [formData,setFormData]=useState({username:'',email:'',password:''});
+    const [username,setUsername]=useState('');
+    const [email,setEmail]=useState('');
+    const [password,setPassword]=useState('');
 
-    const handleChange=(e)=>{
-        const{name,value}=e.target;
-        setFormData(prevState=>({...prevState,[name]:value}));
-    }
-
-   const handleSubmit=(e)=>{
-    e.preventDefault();
-    console.log(formData);
-   }
+    
 
     return(
 <form action="" onSubmit={handleSubmit}>
 
-    <input type="text" name="name" value={formData.username} onChange={handleChange} required/>
-    <input type="text" name="email" value={formData.email} onChange={handleChange} required/>
-    <input type="text" name="password" value={formData.password} onChange={handleChange} required />
+    <input type="text" name="username" value={username} onChange={handleChange} required/>
+    <input type="text" name="email" value={email} onChange={handleChange} required/>
+    <input type="text" name="password" value={password} onChange={handleChange} required />
 
     <button type="submit">Submit</button>
 </form>
