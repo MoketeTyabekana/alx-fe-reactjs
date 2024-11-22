c6f01727fdfd7ea204dbb5c7ce56372eb07f37a3
Mokete <ematyabekana@gmail.com>
2024-11-22 21:44:05 +0200

    Defined a function that can fetch data from the API

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,14 @@
+import { useQuery } from 'react-query';
+
+export default function PostsComponent(){
+
+    const fetchData = async () => {
+        const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+        return res.json();
+    };
+
+
+    return(
+
+    );
+}
