edcaea3948588fbaa63735b128b9c45b410563c6
Mokete <ematyabekana@gmail.com>
2024-11-22 19:21:24 +0200

    Created a funtion handleChange to handle the changes based on the name of the input triggered.

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -2,15 +2,20 @@ import { useState } from "react";
 
 export default function RegistrationForm (){
 
-    const [formData,setFormData]=useState({name:'',email:'',password:''});
+    const [formData,setFormData]=useState({username:'',email:'',password:''});
+
+    const handleChange=(e)=>{
+        const{name,value}=e.target;
+        setFormData(prevState=>({...prevState,[name]:value}));
+    }
 
 
 
     return(
 <form action="">
-    <input type="text" name="name" value={} />
-    <input type="text" name="email" value={} />
-    <input type="text" name="password" value={} />
+    <input type="text" name="name" value={formData.username} />
+    <input type="text" name="email" value={formData.email} />
+    <input type="text" name="password" value={formData.password} />
 
     <button type="submit">Submit</button>
 </form>
