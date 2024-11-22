59c257507de129e8bd2f3b76ae5c1b685904fd9b
Mokete <ematyabekana@gmail.com>
2024-11-22 20:36:21 +0200

    Ccreated a validation sschema useing yup object, to validate user inputs.

---
+++ form-handling-react/src/components/formikForm.js
@@ -0,0 +1,8 @@
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+const validationSchema = Yup.object({
+    name: Yup.string().required('Name is required'),
+    email: Yup.string().email('Invalid email').required('Email is required'),
+    password: Yup.string().required('Password is required')
+});
