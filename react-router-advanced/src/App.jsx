e244be728f5344305d6bbe86f7bd8678e3148184
Mokete <ematyabekana@gmail.com>
2024-11-23 18:49:14 +0200

    Added a ProetetedRoute to check if the user is authenticated

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,56 +1,28 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Navigate, useParams } from 'react-router-dom';
-import { useState } from 'react';
+import { BrowserRouter as Router,Routes, Route, useParams,Switch, Link, useRouteMatch } from 'react-router-dom';
 import Profile from './components/Profile';
 
-// BlogPost Component
-const BlogPost = () => {
-  const { userId } = useParams();
-  return <h3>User ID: {userId}</h3>;
-};
 
-// Protected Route Component
-const ProtectedRoute = ({ isAuthenticated, children }) => {
-  return isAuthenticated ? children : <Navigate to="/login" />;
-};
+const BlogPost=()=>{
+  const {userId}=useParams();
+  return<h3>User ID: {userId}</h3>
+}
 
-// Login Component (Simulating Login)
-const Login = ({ onLogin }) => {
-  return (
-    <div>
-      <h1>Login Page</h1>
-      <button onClick={onLogin}>Simulate Login</button>
-    </div>
-  );
-};
-
-// App Component
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  const handleLogin = () => setIsAuthenticated(true);
+  
 
   return (
-    <Router>
-      <Routes>
-        {/* BlogPost Route */}
-        <Route path="/blog/:userId" element={<BlogPost />} />
-
-        {/* Login Route */}
-        <Route path="/login" element={<Login onLogin={handleLogin} />} />
-
-        {/* Protected Profile Route */}
-        <Route
+    <Switch>
+      <Route
           path="/profile"
           element={
             <ProtectedRoute isAuthenticated={isAuthenticated}>
               <Profile />
             </ProtectedRoute>
           }
-        />
-      </Routes>
-    </Router>
-  );
+      <Route path='/blog/:id' Component={BlogPost}/>
+    </Switch>
+  )
 }
 
 export default App;
