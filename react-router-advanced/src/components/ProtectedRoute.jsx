2d1b31deefa559e17c96162a261e7baf1c009de9
Mokete <ematyabekana@gmail.com>
2024-11-23 17:54:17 +0200

    IMport useAuth and Navigation

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,9 +1,10 @@
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import { Navigate} from 'react-router-dom';
+import { useAuth } from './AuthContext';
 
 
 
 export default function ProtectedRoute(){
     return(
-        
+
     )
 }
