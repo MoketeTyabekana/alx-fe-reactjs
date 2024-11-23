8f6ce36974ad13abec97e99939fd4454613ed81f
Mokete <ematyabekana@gmail.com>
2024-11-23 20:47:46 +0200

    installed babel

--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import TodoList from "../components/TodoList";
+import TodoList from "./components/TodoList";
 import { describe, test, expect } from "@jest/globals";
 
 
