048498038c043d90895f626351d8c44aef4d82b4
Mokete <ematyabekana@gmail.com>
2024-11-23 11:05:30 +0200

    Called the profile insde the main App

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css';
 import { BrowserRouter as Router,Routes, Route, Switch, Link, useRouteMatch } from 'react-router-dom';
+import Profile from './components/Profile';
 
 
 function App() {
  
 
   return (
-    
+    <Profile/>
   )
 }
 
