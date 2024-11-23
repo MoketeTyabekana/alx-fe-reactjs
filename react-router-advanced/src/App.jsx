21df93ad0251092ddb20eacacc51ce66c9ca6006
Mokete <ematyabekana@gmail.com>
2024-11-23 12:41:28 +0200

    define a userId using useParams to access the Route parameters.

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css';
-import { BrowserRouter as Router,Routes, Route, Switch, Link, useRouteMatch } from 'react-router-dom';
+import { BrowserRouter as Router,Routes, Route, useParams,Switch, Link, useRouteMatch } from 'react-router-dom';
 import Profile from './components/Profile';
 
 
 const BlogPost=()=>{
-  
+  const {userId}=useParams();
+  return<h3>User ID: {userId}</h3>
 }
 
 function App() {
