5f8365e04893dba9c4ef6721cc02d0d2600495b4
Mokete <ematyabekana@gmail.com>
2024-12-06 12:18:51 +0200

    imported the data.json into the Home component.

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,4 +1,5 @@
-import {React, useEffect, useState} from "react";
+import React, { useEffect, useState} from "react";
+import data from '../data.json';
 
 export default function HomePage(){
     return(
