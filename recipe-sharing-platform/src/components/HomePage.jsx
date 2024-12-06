d30b38fd0260726193b05e9a1ff2cdec64341309
Mokete <ematyabekana@gmail.com>
2024-12-06 16:04:45 +0200

    Created a card and called the recipe objects from the json data file.

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState} from "react";
 import data from '../data.json';
 
 export default function HomePage(){
-  const [recipe,setRecipes]=useState([]);
+  const [recipes,setRecipes]=useState([]);
 
   useEffect(()=>{
     setRecipes(data);
@@ -13,7 +13,26 @@ export default function HomePage(){
         <div className=" container">
           <h1 className="text-4xl font-bold text-center mb-6">Shared Recipe</h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+                {recipes.map((recipe)=>(
+                    
+                    <div key={recipe.id} 
+                    className="max-w-xs rounded overflow-hidden shadow-lg hover:shadow-xl transform hover:scale-105 duration-300"
+                    >
+                        
+                        <img className="w-full h-48 object-cover" style={{
+                            width:100,height:100
+                        }} src={recipe.image} alt={recipe.title} />
 
+                        <div className="px-4 py-3">
+                          <h2 className="text-xl font-semibold">
+                            {recipe.title}
+                          </h2>
+                          <p className="text-gray-700 mt-2">{recipe.summary}</p>
+                        </div>
+                        
+                    </div> 
+
+                ))}
           </div>
         </div>
     )
