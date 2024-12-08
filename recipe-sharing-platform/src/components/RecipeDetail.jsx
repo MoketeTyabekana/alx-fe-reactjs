d2af5f78204e97e1d65e2f0acc27e4a952a3fc0a
Mokete <ematyabekana@gmail.com>
2024-12-08 18:49:58 +0200

    Created a card component for the recipe details, fetching data from the data.json file.

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -20,6 +20,24 @@ if (!recipe){
     return <p className='text-center text-gray-700 mt-4'>Loading Recipes...</p>
 }
 
+
+return(
+    <div className="container mx-auto p-4">
+       <h1 className="text-3xl font-bold text-center mb-4">{recipe.title}</h1>
+       <img className='w-full h-64 object-cover rounded mb-4' src={recipe.image} alt={recipe.title} />
+       <p className='text-gray-700 mb-4'>{recipe.summary}</p>
+       <h2 className='text-2xl font-semibold mb-2'>Ingredients</h2>
+       <ul className="list-disc list-inside mb-4">
+        {recipe.ingredients?.map((ingredient, index) => (
+          <li key={index}>{ingredient}</li>
+        ))}
+
+        <h2 className="text-2xl font-semibold mb-2">Cooking Instructions</h2>
+        <p className="text-gray-700">{recipe.instructions}</p>
+      </ul>
+    </div>
+
+);
 }
 
 export default RecipeDetail;
