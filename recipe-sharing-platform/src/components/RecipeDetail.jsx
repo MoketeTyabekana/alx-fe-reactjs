fea4832021154376ecee34a6292446af7bc9b40e
Mokete <ematyabekana@gmail.com>
2024-12-08 18:21:09 +0200

    Updated the RecipeDetails component by defining the useState values, and taking the id as a param from the url

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React {useState,useEffect} from 'react';
+import { useParams } from "react-router-dom";
+import  recipeData from '../data.json';
+import { useState } from "react";
 
-function RecipeDetail() {
-  return (
-    <div>RecipeDetail</div>
-  )
+const RecipeDetail =() =>{
+   
+const  {id}=useParams();
+const [recipe,setRecipes]=useState(null);
+
 }
 
 export default RecipeDetail;
