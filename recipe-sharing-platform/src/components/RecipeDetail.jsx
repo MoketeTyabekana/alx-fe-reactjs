f890fb691e743dc92bde0f9f05a1cffe94853d9c
Mokete <ematyabekana@gmail.com>
2024-12-08 18:21:57 +0200

    Removed unnecessary imports.

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -1,7 +1,7 @@
-import React {useState,useEffect} from 'react';
+import React, {useState,useEffect} from 'react';
 import { useParams } from "react-router-dom";
 import  recipeData from '../data.json';
-import { useState } from "react";
+
 
 const RecipeDetail =() =>{
    
